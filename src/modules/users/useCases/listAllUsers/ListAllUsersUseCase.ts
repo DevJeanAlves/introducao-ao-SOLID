d2c8841f56668ef0485be0d24db0c4d228378acf
JeanAlves2021 <jean.pdolsky2@gmail.com>
2022-07-26 00:43:11 -0300

    resolve all tests

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -6,10 +6,21 @@ interface IRequest {
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("User not exists");
+    }
+
+    if (user.admin === false) {
+      throw new Error("Ony admin can acess the list.");
+    }
+
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
